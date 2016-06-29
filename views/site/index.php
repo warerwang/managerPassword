b904f4983c94b12985d37638cd26f163d0e61046
warerwang <warerwang@126.com>
2016-06-29 23:48:41 +0800

    first commit

---
+++ views/site/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = '密码管理';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+    </div>
+
+    <div class="body-content">
+    </div>
+</div>
